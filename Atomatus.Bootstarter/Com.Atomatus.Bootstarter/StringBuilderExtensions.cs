7f626df197fc85f6ae1a58a16deb601e7450fccd
Carlos Matos <chcmatos@hotmail.com>
2021-04-12 14:42:46 -0300

    string builder extensions diagnostic attributes changed

--- Atomatus.Bootstarter/Com.Atomatus.Bootstarter/StringBuilderExtensions.cs
+++ Atomatus.Bootstarter/Com.Atomatus.Bootstarter/StringBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -10,31 +11,31 @@ namespace Com.Atomatus.Bootstarter
 {
     internal static class StringBuilderExtensions
     {
-        public static StringBuilder AppendLineIf(this StringBuilder sb, bool condition, params object[] value)
+        public static StringBuilder AppendLineIf([NotNull] this StringBuilder sb, bool condition, params object[] value)
         {
             return condition ? value.Aggregate(sb, (acc, curr) => acc.Append(curr)).AppendLine() : sb;
         }
 
-        public static StringBuilder AppendIf(this StringBuilder sb, bool condition, params object[] value)
+        public static StringBuilder AppendIf([NotNull] this StringBuilder sb, bool condition, params object[] value)
         {
             return condition ? value.Aggregate(sb, (acc, curr) => acc.Append(curr)) : sb;
         }
 
-        public static StringBuilder AppendOrElse(this StringBuilder sb, string target, string elseOption)
+        public static StringBuilder AppendOrElse([NotNull] this StringBuilder sb, [AllowNull] string target, [NotNull] string elseOption)
         {
             return !string.IsNullOrEmpty(target) ?
                 sb.Append(target) :
                 sb.Append(elseOption);
         }
 
-        public static StringBuilder AppendOrElse(this StringBuilder sb, int? target, int elseOption)
+        public static StringBuilder AppendOrElse([NotNull] this StringBuilder sb, [AllowNull] int? target, int elseOption)
         {
             return target.HasValue && target.Value > 0 ? 
                 sb.Append(target) : 
                 sb.Append(elseOption);
         }
 
-        public static StringBuilder AppendOrThrow(this StringBuilder sb, string target, string errorMessage)
+        public static StringBuilder AppendOrThrow([NotNull] this StringBuilder sb, [AllowNull] string target, [NotNull] string errorMessage)
         {
             return !string.IsNullOrEmpty(target) ? 
                 sb.Append(target) : 
