b0f38f9a882aa54195a10133a2986d351eece41f
Carlos Matos <chcmatos@hotmail.com>
2021-04-21 22:57:24 -0300

    service generation by model in AddService dependency.

--- Atomatus.Bootstarter/Com.Atomatus.Bootstarter/Context/IContextServiceCollection.cs
+++ Atomatus.Bootstarter/Com.Atomatus.Bootstarter/Context/IContextServiceCollection.cs
@@ -1,17 +1,21 @@
-﻿using Com.Atomatus.Bootstarter.Services;
+﻿using Com.Atomatus.Bootstarter.Model;
+using Com.Atomatus.Bootstarter.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
 
 namespace Com.Atomatus.Bootstarter.Context
 {
+    #region Contracts
     /// <summary>
-    /// Specifies the contract for a collection of DBContext service.
+    /// Specifies the contract for a collection of <see cref="IService"/>.
     /// </summary>    
-    public interface IContextServiceCollection
+    public interface IContextServiceOperationCollection
     {
         /// <summary>
         /// <para>
@@ -24,7 +28,7 @@ namespace Com.Atomatus.Bootstarter.Context
         /// <typeparam name="TService">service type</typeparam>
         /// <param name="serviceLifetime">service lifetime mode</param>
         /// <returns>current context service collection instance</returns>
-        IContextServiceCollection AddService<TService>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TService : IService;
+        IContextServiceOperationCollection AddService<TService>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TService : IService;
 
         /// <summary>
         /// <para>
@@ -36,7 +40,7 @@ namespace Com.Atomatus.Bootstarter.Context
         /// </summary>
         /// <typeparam name="TService">service type</typeparam>
         /// <returns>current context service collection instance</returns>
-        IContextServiceCollection AddScoped<TService>() where TService : IService;
+        IContextServiceOperationCollection AddScoped<TService>() where TService : IService;
 
         /// <summary>
         /// <para>
@@ -48,7 +52,7 @@ namespace Com.Atomatus.Bootstarter.Context
         /// </summary>
         /// <typeparam name="TService">service type</typeparam>
         /// <returns>current context service collection instance</returns>
-        IContextServiceCollection AddSingleton<TService>() where TService : IService;
+        IContextServiceOperationCollection AddSingleton<TService>() where TService : IService;
 
         /// <summary>
         /// <para>
@@ -60,11 +64,136 @@ namespace Com.Atomatus.Bootstarter.Context
         /// </summary>
         /// <typeparam name="TService">service type</typeparam>
         /// <returns>current context service collection instance</returns>
-        IContextServiceCollection AddTransient<TService>() where TService : IService;
+        IContextServiceOperationCollection AddTransient<TService>() where TService : IService;
     }
 
-    internal interface IContextServiceTypes : IEnumerable<Type>, IEnumerable { }
+    /// <summary>
+    /// Specifies the contract for a collection of <see cref="IModel{TID}"/>.
+    /// </summary>    
+    public interface IContextModelIdOperationCollection
+    {
+        /// <summary>
+        /// <para>
+        /// Generate and Add dynamic service from specified contract IModel to target dbcontext.
+        /// </para>
+        /// <para>
+        /// <i>Obs.: The TModel can be assigned as property in target context.</i>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TModel">model to generate service type</typeparam>
+        /// <typeparam name="TID">id model type</typeparam>
+        /// <param name="serviceLifetime">service lifetime mode</param>
+        /// <returns>current context service collection instance</returns>
+        IContextModelIdOperationCollection AddServiceTo<TModel, TID>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TModel : IModel<TID>;
 
+        /// <summary>
+        /// <para>
+        /// Generate and Add dynamic service from specified contract IModel to target dbcontext as <see cref="ServiceLifetime.Scoped"/>.
+        /// </para>
+        /// <para>
+        /// <i>Obs.: The TModel can be assigned as property in target context.</i>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TModel">model to generate service type</typeparam>
+        /// <typeparam name="TID">id model type</typeparam>
+        /// <returns>current context service collection instance</returns>
+        IContextModelIdOperationCollection AddScoped<TModel, TID>() where TModel : IModel<TID>;
+
+        /// <summary>
+        /// <para>
+        /// Generate and Add dynamic service from specified contract IModel to target dbcontext as <see cref="ServiceLifetime.Singleton"/>.
+        /// </para>
+        /// <para>
+        /// <i>Obs.: The TModel can be assigned as property in target context.</i>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TModel">model to generate service type</typeparam>
+        /// <typeparam name="TID">id model type</typeparam>
+        /// <returns>current context service collection instance</returns>
+        IContextModelIdOperationCollection AddSingleton<TModel, TID>() where TModel : IModel<TID>;
+
+        /// <summary>
+        /// <para>
+        /// Generate and Add dynamic service from specified contract IModel to target dbcontext as <see cref="ServiceLifetime.Transient"/>.
+        /// </para>
+        /// <para>
+        /// <i>Obs.: The TModel can be assigned as property in target context.</i>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TModel">model to generate service type</typeparam>
+        /// <typeparam name="TID">id model type</typeparam>
+        /// <returns>current context service collection instance</returns>
+        IContextModelIdOperationCollection AddTransient<TModel, TID>() where TModel : IModel<TID>;
+    }
+
+    /// <summary>
+    /// Specifies the contract for a collection of <see cref="IModel"/>.
+    /// </summary>    
+    public interface IContextModelOperationCollection
+    {
+        /// <summary>
+        /// <para>
+        /// Generate and Add dynamic service from specified contract IModel to target dbcontext.
+        /// </para>
+        /// <para>
+        /// <i>Obs.: The TModel can be assigned as property in target context.</i>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TModel">model to generate service type</typeparam>
+        /// <typeparam name="TID">id model type</typeparam>
+        /// <param name="serviceLifetime">service lifetime mode</param>
+        /// <returns>current context service collection instance</returns>
+        IContextModelOperationCollection AddServiceTo<TModel>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TModel : IModel;
+
+        /// <summary>
+        /// <para>
+        /// Generate and Add dynamic service from specified contract IModel to target dbcontext as <see cref="ServiceLifetime.Scoped"/>.
+        /// </para>
+        /// <para>
+        /// <i>Obs.: The TModel can be assigned as property in target context.</i>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TModel">model to generate service type</typeparam>
+        /// <typeparam name="TID">id model type</typeparam>
+        /// <returns>current context service collection instance</returns>
+        IContextModelOperationCollection AddScoped<TModel>() where TModel : IModel;
+
+        /// <summary>
+        /// <para>
+        /// Generate and Add dynamic service from specified contract IModel to target dbcontext as <see cref="ServiceLifetime.Singleton"/>.
+        /// </para>
+        /// <para>
+        /// <i>Obs.: The TModel can be assigned as property in target context.</i>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TModel">model to generate service type</typeparam>
+        /// <typeparam name="TID">id model type</typeparam>
+        /// <returns>current context service collection instance</returns>
+        IContextModelOperationCollection AddSingleton<TModel>() where TModel : IModel;
+
+        /// <summary>
+        /// <para>
+        /// Generate and Add dynamic service from specified contract IModel to target dbcontext as <see cref="ServiceLifetime.Transient"/>.
+        /// </para>
+        /// <para>
+        /// <i>Obs.: The TModel can be assigned as property in target context.</i>
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TModel">model to generate service type</typeparam>
+        /// <typeparam name="TID">id model type</typeparam>
+        /// <returns>current context service collection instance</returns>
+        IContextModelOperationCollection AddTransient<TModel>() where TModel : IModel;
+    }
+
+    /// <summary>
+    /// Specifies the contract for a collection of DBContext service.
+    /// </summary>    
+    public interface IContextServiceCollection : IContextServiceOperationCollection, IContextModelIdOperationCollection, IContextModelOperationCollection { }
+
+    internal interface IContextServiceTypes : IEnumerable<Type>, IEnumerable { }
+    #endregion
+
+    #region Implementations
     internal sealed class ContextServiceTypeCollection : ContextServiceCollection<ContextBase> 
     {
         public ContextServiceTypeCollection() : base() { }
@@ -92,8 +221,8 @@ namespace Com.Atomatus.Bootstarter.Context
             this.serviceTypes = new HashSet<Type>();
         }
 
-        #region IContextServiceCollection
-        public IContextServiceCollection AddService<TService>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TService : IService
+        #region IContextServiceOperationCollection
+        public IContextServiceOperationCollection AddService<TService>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TService : IService
         {
             this.RequireNonDisposed();
             this.services?.AddService<TContext, TService>(serviceLifetime);
@@ -101,20 +230,52 @@ namespace Com.Atomatus.Bootstarter.Context
             return this;
         }
 
-        public IContextServiceCollection AddScoped<TService>() where TService : IService
+        public IContextServiceOperationCollection AddScoped<TService>() where TService : IService => AddService<TService>(ServiceLifetime.Scoped);
+
+        public IContextServiceOperationCollection AddSingleton<TService>() where TService : IService => AddService<TService>(ServiceLifetime.Singleton);
+
+        public IContextServiceOperationCollection AddTransient<TService>() where TService : IService => AddService<TService>(ServiceLifetime.Transient);
+        #endregion
+
+        #region IContextModelOperationCollection
+        public IContextModelIdOperationCollection AddServiceTo<TModel, TID>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TModel : IModel<TID>
         {
-            return AddService<TService>(ServiceLifetime.Scoped);
+            this.AddService<IServiceCrud<TModel, TID>>(serviceLifetime);
+            this.AddService<IServiceCrudAsync<TModel, TID>>(serviceLifetime);            
+            return this;
         }
 
-        public IContextServiceCollection AddSingleton<TService>() where TService : IService
+        IContextModelIdOperationCollection IContextModelIdOperationCollection.AddScoped<TModel, TID>() => AddServiceTo<TModel, TID>(ServiceLifetime.Scoped);
+
+        IContextModelIdOperationCollection IContextModelIdOperationCollection.AddSingleton<TModel, TID>() => AddServiceTo<TModel, TID>(ServiceLifetime.Singleton);
+
+        IContextModelIdOperationCollection IContextModelIdOperationCollection.AddTransient<TModel, TID>() => AddServiceTo<TModel, TID>(ServiceLifetime.Transient);
+        #endregion
+
+        #region IContextModelOperationCollection
+        public IContextModelOperationCollection AddServiceTo<TModel>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TModel : IModel
         {
-            return AddService<TService>(ServiceLifetime.Singleton);
+            Type mType  = typeof(TModel);
+            Type gmType = mType.GetGenericInterfaceType(typeof(IModel<>)) ?? throw new InvalidOperationException(
+                $"The Entity type {typeof(TModel).Name} does not " +
+                $"implements the interface \"{typeof(IModel<>)}\". " +
+                $"So is not possible identify your ID type!");
+
+            Type idType = gmType.GetGenericArguments().First();
+
+            Type operModelIdType = typeof(IContextModelIdOperationCollection);
+            var res = operModelIdType.GetMethod(nameof(AddServiceTo), BindingFlags.Public | BindingFlags.Instance)
+                .MakeGenericMethod(mType, idType)
+                .Invoke(this, new object[] { serviceLifetime });
+
+            return res as IContextModelOperationCollection;
         }
 
-        public IContextServiceCollection AddTransient<TService>() where TService : IService
-        {
-            return AddService<TService>(ServiceLifetime.Transient);
-        }
+        IContextModelOperationCollection IContextModelOperationCollection.AddScoped<TModel>() => AddServiceTo<TModel>(ServiceLifetime.Scoped);
+
+        IContextModelOperationCollection IContextModelOperationCollection.AddSingleton<TModel>() => AddServiceTo<TModel>(ServiceLifetime.Singleton);
+
+        IContextModelOperationCollection IContextModelOperationCollection.AddTransient<TModel>() => AddServiceTo<TModel>(ServiceLifetime.Transient);
         #endregion
 
         #region IContextServiceTypes
@@ -159,4 +320,5 @@ namespace Com.Atomatus.Bootstarter.Context
         }
         #endregion
     }
+    #endregion
 }
--- Atomatus.Bootstarter/Com.Atomatus.Bootstarter/Services/ServiceExtensions.cs
+++ Atomatus.Bootstarter/Com.Atomatus.Bootstarter/Services/ServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Com.Atomatus.Bootstarter.Context;
+using Com.Atomatus.Bootstarter.Model;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Diagnostics.CodeAnalysis;
@@ -10,6 +11,7 @@ namespace Com.Atomatus.Bootstarter.Services
     /// </summary>
     public static class ServiceExtensions
     {
+        #region IService
         /// <summary>
         /// Create a dynamic service type to specified service type (<typeparamref name="TService"/>)
         /// and target context <typeparamref name="TContext"/>. 
@@ -29,24 +31,24 @@ namespace Com.Atomatus.Bootstarter.Services
         }
 
         /// <summary>
-        /// Create a dynamic service type to specified service type (<typeparamref name="TService"/>)
+        /// Create a dynamic service type to specified service type (<typeparamref name="TModel"/>)
         /// and target context <typeparamref name="TContext"/>. 
         /// Then, register it to service collection how <paramref name="serviceLifetime"/> defined it.
         /// </summary>
         /// <typeparam name="TContext">database context type.</typeparam>
-        /// <typeparam name="TService">service interface type.</typeparam>
+        /// <typeparam name="TModel">service interface type.</typeparam>
         /// <param name="services">current service collection.</param>
         /// <param name="serviceLifetime">The lifetime with which to register the Service in the container.</param>
         /// <returns>current service collection</returns>
-        public static IServiceCollection AddService<TContext, TService>([NotNull] this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        public static IServiceCollection AddService<TContext, TModel>([NotNull] this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
             where TContext : ContextBase
-            where TService : IService
+            where TModel : IService
         {
             return serviceLifetime switch
             {
-                ServiceLifetime.Singleton   => CreateAndAddServiceDynamicType<TContext, TService>(services.AddSingleton),
-                ServiceLifetime.Scoped      => CreateAndAddServiceDynamicType<TContext, TService>(services.AddScoped),
-                ServiceLifetime.Transient   => CreateAndAddServiceDynamicType<TContext, TService>(services.AddTransient),
+                ServiceLifetime.Singleton   => CreateAndAddServiceDynamicType<TContext, TModel>(services.AddSingleton),
+                ServiceLifetime.Scoped      => CreateAndAddServiceDynamicType<TContext, TModel>(services.AddScoped),
+                ServiceLifetime.Transient   => CreateAndAddServiceDynamicType<TContext, TModel>(services.AddTransient),
                 _ => throw new NotImplementedException(),
             };
         }
@@ -98,5 +100,79 @@ namespace Com.Atomatus.Bootstarter.Services
         {
             return CreateAndAddServiceDynamicType<TContext, TService>(services.AddTransient);
         }
+        #endregion
+
+        #region IModel
+        /// <summary>
+        /// Create a dynamic service CRUD type (<see cref="IServiceCrud{TEntity, ID}"/>) to specified model type (<typeparamref name="TModel"/>)
+        /// and target context <typeparamref name="TContext"/>. 
+        /// Then, register it to service collection how <paramref name="serviceLifetime"/> defined it.
+        /// </summary>
+        /// <typeparam name="TContext">database context type.</typeparam>
+        /// <typeparam name="TModel">model type.</typeparam>
+        /// <typeparam name="TID">id model type.</typeparam>
+        /// <param name="services">current service collection.</param>
+        /// <param name="serviceLifetime">The lifetime with which to register the Service in the container.</param>
+        /// <returns>current service collection</returns>
+        public static IServiceCollection AddService<TContext, TModel, TID>([NotNull] this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+            where TContext : ContextBase
+            where TModel : IModel<TID>
+        {
+            return services
+                .AddService<TContext, IServiceCrud<TModel, TID>>(serviceLifetime)
+                .AddService<TContext, IServiceCrudAsync<TModel, TID>>(serviceLifetime);
+        }
+
+        /// <summary>
+        /// Create a dynamic service CRUD type (<see cref="IServiceCrud{TEntity, ID}"/>) to specified model type (<typeparamref name="TModel"/>)
+        /// and target context <typeparamref name="TContext"/>. 
+        /// Then, register it to service collection how scoped service type.
+        /// </summary>
+        /// <typeparam name="TContext">database context type.</typeparam>
+        /// <typeparam name="TModel">model type.</typeparam>
+        /// <typeparam name="TID">id model type.</typeparam>
+        /// <param name="services">current service collection</param>
+        /// <returns>current service collection</returns>
+        public static IServiceCollection AddServiceScoped<TContext, TModel, TID>([NotNull] this IServiceCollection services)
+            where TContext : ContextBase
+            where TModel : IModel<TID>
+        {
+            return CreateAndAddServiceDynamicType<TContext, IServiceCrud<TModel, TID>>(services.AddScoped);
+        }
+
+        /// <summary>
+        /// Create a dynamic service CRUD type (<see cref="IServiceCrud{TEntity, ID}"/>) to specified model type (<typeparamref name="TModel"/>)
+        /// and target context <typeparamref name="TContext"/>. 
+        /// Then, register it to service collection how singleton service type.
+        /// </summary>
+        /// <typeparam name="TContext">database context type.</typeparam>
+        /// <typeparam name="TModel">model type.</typeparam>
+        /// <typeparam name="TID">id model type.</typeparam>
+        /// <param name="services">current service collection</param>
+        /// <returns>current service collection</returns>
+        public static IServiceCollection AddServiceSingleton<TContext, TModel, TID>([NotNull] this IServiceCollection services)
+            where TContext : ContextBase
+            where TModel : IModel<TID>
+        {
+            return CreateAndAddServiceDynamicType<TContext, IServiceCrud<TModel, TID>>(services.AddSingleton);
+        }
+
+        /// <summary>
+        /// Create a dynamic service CRUD type (<see cref="IServiceCrud{TEntity, ID}"/>) to specified model type (<typeparamref name="TModel"/>)
+        /// and target context <typeparamref name="TContext"/>. 
+        /// Then, register it to service collection how transient service type.
+        /// </summary>
+        /// <typeparam name="TContext">database context type.</typeparam>
+        /// <typeparam name="TModel">model type.</typeparam>
+        /// <typeparam name="TID">id model type.</typeparam>
+        /// <param name="services">current service collection</param>
+        /// <returns>current service collection</returns>
+        public static IServiceCollection AddServiceTransient<TContext, TModel, TID>([NotNull] this IServiceCollection services)
+            where TContext : ContextBase
+            where TModel : IModel<TID>
+        {
+            return CreateAndAddServiceDynamicType<TContext, IServiceCrud<TModel, TID>>(services.AddTransient);
+        }
+        #endregion
     }
 }
