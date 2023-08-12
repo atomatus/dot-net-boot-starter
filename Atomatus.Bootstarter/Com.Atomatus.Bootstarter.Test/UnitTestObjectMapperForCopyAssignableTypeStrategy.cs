1f4f6a48d169492db17b2eb847875f350bf9e5b7
Carlos Matos <chcmatos@hotmail.com>
2023-08-12 18:23:13 -0300

    Implementing more unit tests.

--- Atomatus.Bootstarter/Com.Atomatus.Bootstarter.Test/UnitTestObjectMapperForCopyAssignableTypeStrategy.cs
+++ Atomatus.Bootstarter/Com.Atomatus.Bootstarter.Test/UnitTestObjectMapperForCopyAssignableTypeStrategy.cs
@@ -7,18 +7,20 @@ namespace Com.Atomatus.Bootstarter.Test
         [Fact]
         public void Utils_ObjectMapper_Copy_Assinable_Type_Strategy_Successfully()
         {
-            A a = new() { X = 1, Y = 2 };
+            A a = new() { X = 1, Y = 2, Uuid = Guid.NewGuid() };
             B b = new() { Z = 3};
             Assert.True(ObjectMapper.Copy(a, b));
             Assert.Equal(a.X, b.X);
             Assert.Equal(a.Y, b.Y);
+            Assert.Equal(a.Uuid, b.Uuid);
             Assert.Equal(3, b.Z);
 
-            a = new() { X = 3, Y = 4 };
-            b = new() { Z = 5 };
+            a = new() { X = 3, Y = 4, Uuid = Guid.NewGuid() };
+            b = new() { X = 5, Y = 6, Uuid = Guid.NewGuid(), Z = 5 };
             Assert.True(ObjectMapper.Copy(b, a));
             Assert.Equal(a.X, b.X);
             Assert.Equal(a.Y, b.Y);
+            Assert.Equal(a.Uuid, b.Uuid);
             Assert.Equal(5, b.Z);
         }
 
@@ -26,6 +28,8 @@ namespace Com.Atomatus.Bootstarter.Test
         {
             public int X { get; set; }
             public int Y { get; set; }
+
+            public Guid Uuid { get; set; }
         }
 
         class B : A
---
+++ Atomatus.Bootstarter/Com.Atomatus.Bootstarter.Test/UnitTestObjectMapperForDTO.cs
@@ -0,0 +1,64 @@
+﻿using Com.Atomatus.Bootstarter.Util;
+
+namespace Com.Atomatus.Bootstarter.Test
+{
+    public class UnitTestObjectMapperForDTO
+    {
+        [Fact]
+        public void Utils_ObjectMapper_Parse_For_DTO_Successfully()
+        {
+            User user = new()
+            {
+                Uuid = Guid.NewGuid(),
+                Username = "test",
+                Password = "123456",
+            };
+
+            var dto = ObjectMapper.Parse<UserDTO>(user);
+
+            Assert.NotNull(user);
+            Assert.NotNull(dto);
+            Assert.Equal(user.Uuid, dto.Uuid);
+            Assert.Equal(user.Username, dto.Username);
+        }
+
+        interface IModel
+        {
+            Guid Uuid { get; set; }
+        }
+
+        interface IUser : IModel
+        {
+            string? Username { get; set; }
+        }
+
+        interface IUserAuth: IUser
+        {
+            public string? Password { get; set; }
+        }
+
+        class AuditableModel : IModel
+        {
+            public Guid Uuid { get; set; }
+
+            public DateTime CreatedAt { get; set; } = DateTime.Now;
+        }
+
+        class User : AuditableModel, IUserAuth
+        {
+            public string? Username { get; set; }
+
+            public string? Password { get; set; }
+        }
+
+        class BaseDTO
+        {
+            public Guid Uuid { get; set; }
+        }
+
+        class UserDTO : BaseDTO, IUser
+        {
+            public string? Username { get; set; }
+        }
+    }
+}
