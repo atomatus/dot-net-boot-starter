be4d21e73876e8570dc79217382ceabbd62e99fe
carlosmatos <chcmatos@hotmail.com>
2023-08-24 16:10:08 -0300

    Implementing repositories

---
+++ Atomatus.Bootstarter/Com.Atomatus.Bootstarter/Repositories/IRepository.Crud.cs
@@ -0,0 +1,23 @@
+﻿using Com.Atomatus.Bootstarter.Model;
+
+namespace Com.Atomatus.Bootstarter.Repositories
+{
+    /// <summary>
+    /// Entity Repository CRUD for DbContext.
+    /// </summary>
+    /// <typeparam name="TEntity">entity type</typeparam>
+    public interface IRepositoryCrud<TEntity>
+        : IRepository<TEntity>, ICrud<TEntity>
+        where TEntity : IModel
+    { }
+
+    /// <summary>
+    /// Entity Repository CRUD for DbContext.
+    /// </summary>
+    /// <typeparam name="TEntity">entity type</typeparam>
+    /// <typeparam name="ID">entity id type</typeparam>
+    public interface IRepositoryCrud<TEntity, ID>
+        : IRepositoryCrud<TEntity>, IRepository<TEntity, ID>, ICrud<TEntity, ID>
+        where TEntity : IModel<ID>
+    { }
+}
--- Atomatus.Bootstarter/Com.Atomatus.Bootstarter/Services/IService.Crud.cs
+++ Atomatus.Bootstarter/Com.Atomatus.Bootstarter/Services/IService.Crud.cs
@@ -6,8 +6,8 @@ namespace Com.Atomatus.Bootstarter.Services
     /// Entity service CRUD for DbContext.
     /// </summary>
     /// <typeparam name="TEntity">entity type</typeparam>
-    public interface IServiceCrud<TEntity> :
-        IService<TEntity>, ICrud<TEntity>
+    public interface IServiceCrud<TEntity>
+        : IService<TEntity>, ICrud<TEntity>
         where TEntity : IModel { }
 
     /// <summary>
@@ -15,7 +15,7 @@ namespace Com.Atomatus.Bootstarter.Services
     /// </summary>
     /// <typeparam name="TEntity">entity type</typeparam>
     /// <typeparam name="ID">entity id type</typeparam>
-    public interface IServiceCrud<TEntity, ID> :
-        IServiceCrud<TEntity>, IService<TEntity, ID>, ICrud<TEntity, ID>
+    public interface IServiceCrud<TEntity, ID>
+        : IServiceCrud<TEntity>, IService<TEntity, ID>, ICrud<TEntity, ID>
         where TEntity : IModel<ID> { }
 }
